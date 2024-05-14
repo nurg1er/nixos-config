02547218c42691b4a10012e20ac760c13fa82556
nurg1er <nurgler@yandex.ru>
2024-05-14 23:35:41 +0500

    feat: hyprland?

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -11,13 +11,15 @@
   # You can import other home-manager modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
-    outputs.homeManagerModules.default
+    # outputs.homeManagerModules.example
 
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
+    ./features/bash
+    ./features/hyprland
   ];
 
   nixpkgs = {
--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -2,6 +2,5 @@
 # These should be stuff you would like to share with others, not your personal configurations.
 {
   # List your module files here
-  bash = import ./bash.nix;
-  hyprland = import ./hyprland.nix;
+  # my-module = import ./my-module.nix;
 }
