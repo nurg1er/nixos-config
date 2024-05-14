ff2d2f30f2bf5d03c6464d97bbad4e8f49918a0a
nurg1er <nurgler@yandex.ru>
2024-05-14 19:07:19 +0000

    fix: card?

--- home-manager/features/hyprland.nix
+++ home-manager/features/hyprland.nix
@@ -5,15 +5,15 @@
   wayland.windowManager.hyprland.settings = {
     "$mod" = "SUPER";
     "$alt_mod" = "ALT";
-    "monitor" = "eDP-1,2560x1600@96,0x0,1.6";
+#    "monitor" = "eDP-1,2560x1600@96,0x0,1.6";
 
     env = [
       "NIXOS_OZONE_WL, 1"
-	  "XDG_CURRENT_DESKTOP, Hyprland"
-	  "XDG_SESSION_TYPE, wayland"
-	  "XDG_SESSION_DESKTOP, Hyprland"
-	  "WLR_DRM_DEVICES,/dev/dri/card0"
-	  "QT_QPA_PLATFORMTHEME, qt6ct"
+      "XDG_CURRENT_DESKTOP, Hyprland"
+      "XDG_SESSION_TYPE, wayland"
+      "XDG_SESSION_DESKTOP, Hyprland"
+      "WLR_DRM_DEVICES,/dev/dri/by-path/pci-0000:69:00.0-card"
+      "QT_QPA_PLATFORMTHEME, qt6ct"
     ];
 
     xwayland = {
@@ -27,30 +27,30 @@
     bind = [
       # basics
       "$mod, RETURN, exec, alacritty"
-	  "$mod, C, killactive"
-	  "$mod, M, exit"
-
-  	  # navigation
-  	  "$mod, left, movefocus, l"
-	  "$mod, right, movefocus, r"
-	  "$mod, up, movefocus, u"
-	  "$mod, down, movefocus, d"
-	  "$mod, h, movefocus, l"
-	  "$mod, l, movefocus, r"
-	  "$mod, k, movefocus, u"
-	  "$mod, j, movefocus, d"
-
-  	  "$mod SHIFT, left, movewindow, l"
-  	  "$mod SHIFT, right, movewindow, r"
-	  "$mod SHIFT, up, movewindow, u"
-	  "$mod SHIFT, down, movewindow, d"
-	  "$mod SHIFT, h, movewindow, l"
-	  "$mod SHIFT, l, movewindow, r"
-	  "$mod SHIFT, k, movewindow, u"
-	  "$mod SHIFT, j, movewindow, d"
-
-  	  "$mod, Tab, cyclenext,"
-  	  "$mod, Tab, bringactivetotop,"
+      "$mod, C, killactive"
+      "$mod, M, exit"
+    
+      # navigation
+      "$mod, left, movefocus, l"
+      "$mod, right, movefocus, r"
+      "$mod, up, movefocus, u"
+      "$mod, down, movefocus, d"
+      "$mod, h, movefocus, l"
+      "$mod, l, movefocus, r"
+      "$mod, k, movefocus, u"
+      "$mod, j, movefocus, d"
+    
+      "$mod SHIFT, left, movewindow, l"
+      "$mod SHIFT, right, movewindow, r"
+      "$mod SHIFT, up, movewindow, u"
+      "$mod SHIFT, down, movewindow, d"
+      "$mod SHIFT, h, movewindow, l"
+      "$mod SHIFT, l, movewindow, r"
+      "$mod SHIFT, k, movewindow, u"
+      "$mod SHIFT, j, movewindow, d"
+    
+      "$mod, Tab, cyclenext,"
+      "$mod, Tab, bringactivetotop,"
 
       "$alt_mod, Tab, workspace, e+1"
 
